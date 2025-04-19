4d66992c04584c62387bb2dcf6c59f68872e1c2e
Dan Benston <dbenston07@gmail.com>
2025-04-19 18:06:13 -0500

    Initial Commit with Spring Boot template.

---
+++ src/main/java/com/worldbuilder/worldbuilder_suite/WorldbuilderSuiteApplication.java
@@ -0,0 +1,13 @@
+package com.worldbuilder.worldbuilder_suite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldbuilderSuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldbuilderSuiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worldbuilder/worldbuilder_suite/WorldbuilderSuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldbuilder.worldbuilder_suite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldbuilderSuiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
